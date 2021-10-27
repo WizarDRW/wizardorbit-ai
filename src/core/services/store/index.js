1bb4bfa35c47f36a5b1416e4ef2cce9a29f7eed8
Burak <wizard1456@hotmail.com>
2021-10-27 23:51:13 +0300

    Store ready.

--- src/core/services/store/index.js
+++ src/core/services/store/index.js
@@ -1,11 +1,17 @@
 import { createStore } from 'vuex'
 
+import config from "./modules/config.module"
+import math from "./modules/math.module"
+import option from "./modules/option.module"
+
 export default createStore({
   state: {},
   getters: {},
   actions: {},
   mutations: {},
   modules: {
-    
+    config,
+    option,
+    math
   }
 })
